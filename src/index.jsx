5f69c8d6e86ca4e22f165088e71fc191ba723bd3
Sergii-Drozdiuk <drozduyk@gmail.com>
2023-11-21 12:35:36 +0200

    fix-404

--- src/index.jsx
+++ src/index.jsx
@@ -4,16 +4,16 @@ import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import { store, persistor } from './redux/store';
 import { PersistGate } from 'redux-persist/integration/react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/goit-react-hw-08-phonebook'>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
